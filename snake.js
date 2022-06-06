5cb51500e0f781795a5b99de9e17d47158fbf13e
Felix Kleeschulte <felix.kleeschulte@gmail.com>
2022-06-06 15:37:39 +0200

    added randomized food placement

--- snake.js
+++ snake.js
@@ -1,21 +1,35 @@
 let canvas = document.getElementById('canvas_playArea');
 let ctx = canvas.getContext("2d");
-let rows = 10;
-let columns = 10;
-let box_height;
-let box_width;
+let rows = 20;
+let columns = 20;
+let box_height = canvas.height / rows;;
+let box_width = canvas.width / columns;
+let snake = [{ x: 5, y: 5 }];
+let food;
 
+//generateRandomCoordinates().x_value;
+//food = generateRandomCoordinates();
+placeFood();
 draw();
 
-//function for initializing the game
-function initializeGame() {
-    box_height = canvas.height / rows;
-    box_width = canvas.width / columns;
-}
 
-//function for drawing canvas and elements within canvas
+
+//function for drawing canvas background and elements within canvas
 function draw() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawElementOnCanvas('green', food.x, food.y)
+    drawElementOnCanvas('yellow', 1, 1);
+}
+
+//generic function for drawing elements on the canvas, like food or snake parts
+function drawElementOnCanvas(color, x, y) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * box_width, y * box_height, box_width, box_height)
+}
+
+//function for generating new food coordinates
+function placeFood() {
+    food = { x: Math.floor(Math.random() * columns), y: Math.floor(Math.random() * rows) };
 }
 
