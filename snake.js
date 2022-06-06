2f364b4f76b361a6fc3d801b88fed0bce6a69982
Felix Kleeschulte <felix.kleeschulte@gmail.com>
2022-06-06 14:45:30 +0200

    added rows and columns

--- snake.js
+++ snake.js
@@ -1,11 +1,21 @@
 let canvas = document.getElementById('canvas_playArea');
 let ctx = canvas.getContext("2d");
+let rows = 10;
+let columns = 10;
+let box_height;
+let box_width;
 
 draw();
 
-//function for drawing of canvas and elements within canvas
+//function for initializing the game
+function initializeGame() {
+    box_height = canvas.height / rows;
+    box_width = canvas.width / columns;
+}
+
+//function for drawing canvas and elements within canvas
 function draw() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 }
+
